3db6b2f63941f2e46751332ab2c59340137e4b4b
Luca Hostettler <hostettler.luca1896@gmail.com>
2019-06-29 11:18:53 +0200

    Init Skyroute

--- exercises/decision_trees/main.py
+++ exercises/decision_trees/main.py
@@ -1,18 +1,19 @@
-import math
 import matplotlib
 import random
 import pandas
+import sklearn
 
 from sklearn import preprocessing
+from sklearn import ensemble
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 
-
-df = pandas.read_csv('csv.data', header=0)
+df = pandas.read_csv('C:\\_dev\\source\\git\\githucar.data', header=0)
 df.info()
-
 
 df = df.replace('vhigh', 4)
 df = df.replace('high', 3)
-
 
 car = df.values
 
@@ -20,15 +21,14 @@ X, y = car[:, :6], car[:, 6]
 
 X, y = X.astype(int), y.astype(int)
 
-
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(
-    X, y, test_size=0.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3,)
 clf = ensemble.RandomForestClassifier(n_estimators=500)
 
 clf.fit(X_train, y_train)
-RandomForestClassifier(bootstrap=True, compute_importances=None,
+RandomForestClassifier(bootstrap=True,
                        criterion='gini', max_depth=None, max_features='auto',
-                       max_leaf_nodes=None, min_density=None, min_samples_leaf=1,
+                       max_leaf_nodes=None,  min_samples_leaf=1,
                        min_samples_split=2, n_estimators=500, n_jobs=1,
                        oob_score=False, random_state=None, verbose=0)
 clf.score(X_test, y_test)
